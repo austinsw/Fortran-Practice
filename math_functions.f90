41f5eb4ae207121e00928d3ac72f900e1f273c22
austinsw <42607409+austinsw@users.noreply.github.com>
2022-05-25 21:53:42 +0800

    Math Functions

---
+++ math_functions.f90
@@ -0,0 +1,20 @@
+program fortrantut
+  implicit none
+  print "(a10, i1)", "ABS(-1) = ", ABS(-1)
+  print "(a11, f3.1)", "SQRT(81) = ", SQRT(81.0)
+  print "(a9, f7.5)", "EXP(1) = ", EXP(1.0)
+  print "(a12, f7.5)", "LOG(2.71) = ", LOG(2.71)
+  print "(a12, i1)", "INT(2.71) = ", INT(2.71)
+  print "(a13, i1)", "NINT(2.71) = ", NINT(2.71)
+  print "(a14, i1)", "FLOOR(2.71) = ", FLOOR(2.71)
+  print "(a15, f3.1)", "MAX(2.7,3.4) = ", MAX(2.7,3.4)
+  print "(a15, f3.1)", "MIN(2.71,3.4) = ", MIN(2.71,3.4)
+
+  print "(a14, f3.1)", "SIN(1.5708) = ", SIN(1.5708)
+  print "(a14, f3.1)", "COS(1.5708) = ", COS(1.5708)
+  print "(a14, f3.1)", "TAN(1.5708) = ", TAN(1.5708)
+  print "(a10, f3.1)", "ASIN(0) = ", ASIN(0.0)
+  print "(a10, f3.1)", "ACOS(0) = ", ACOS(0.0)
+  print "(a10, f3.1)", "ATAN(0) = ", ATAN(0.0)
+
+ end program fortrantut
